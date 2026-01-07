81838e305a4c063e1e33fd0b9ef66096ef941201
Pallavi_addala <151432100+Pallaviaddal@users.noreply.github.com>
2026-01-07 13:59:52 +0530

    Change logo image width to 50 pixels
    Updated logo image width to 50 pixels.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -20,7 +20,7 @@ function Header() {
       <div className="flex justify-between items-center">
         {/* Logo */}
         <Link to="/" className="flex items-center">
-          <img src="/logo.png" alt="Artivana Logo"  className="object-contain w-40" />
+          <img src="/logo.png" alt="Artivana Logo" width = '50' className="object-contain  />
          
         </Link>
 
